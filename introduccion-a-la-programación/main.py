411393c864e7d30b4cd4688efc18b74038c88232
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-05-29 19:43:53 -0400

    feat: script de apoyo - clase presencial

---
+++ introduccion-a-la-programación/main.py
@@ -0,0 +1,40 @@
+# Tipos de datos
+cadenaDeCaracteres1 = "Aquí puedo escribir un valor de tipo texto"
+cadenaDeCaracteres2 = 'Y con comilla simple :D'
+# str()
+
+numeroEntero = 0
+# int()
+
+numeroDecimal = 1.5
+# float()
+
+verdad = True
+mentira = False
+# bool()
+
+# Salida de datos
+print()
+print("Soy una cadena de caracteres")
+print(16)
+print(1.5)
+
+# Matematicas con Python
+print("Suma: ", 9+10)
+print("Resta:", 2-1)
+print("Multiplicación: ", 2*2)
+print("División: ", 4/2)
+print("Elevar: ", 2**3)
+print("De 4/2 me queda: ", 4%2)
+print("De 7/2 me queda: ", 7%2)
+
+# Entrada de datos
+datoDeEntrada = input("Ingresa una contraseña: ")
+print("Adiviné tu contraseña, es: ", datoDeEntrada)
+
+
+# Convertir los tipos de dato
+a = int(input("Ingresa un número entero: "))
+b = int(input("Ingresa otro número entero: "))
+
+print("El resultado de la suma: ", a+b)
